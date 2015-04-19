0e8f369e607ab67258df2d185cffc808a8fc1154
Cole N <cd.nakadate@gmail.com>
2015-04-19 18:06:40 +0000

    bycrpt backend stuff, config links and routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131181535) do
+ActiveRecord::Schema.define(version: 20150419164848) do
 
   create_table "criteria", force: true do |t|
     t.string   "school_name"
@@ -51,5 +51,15 @@ ActiveRecord::Schema.define(version: 20150131181535) do
     t.text     "pr9"
     t.text     "note"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
